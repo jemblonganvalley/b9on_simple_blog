25f3411110c3507d20fde53194a207207beb40a9
jemblonganvalley <jemblonganvalley@gmail.com>
2022-09-22 22:14:17 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,9 +16,9 @@ export default function Home(){
             app.innerHTML += `
                 <a href="/details?id=${e.id}" class="bg-white p-6 flex flex-col rounded-md my-6 max-w-[700px] shadow-md mx-auto">
                     <h1 class="text-2xl"> ${e.judul} </h1>
-                    <small> ${e.author} - ${e.createdAt} </small>
+                    <small class="text-slate-500"> ${e.author} - ${e.createdAt} </small>
                     <img src="${e.img}" alt="${e.judul}" class="h-[280px] object-cover my-4 rounded-lg" />
-                    <p> ${e.content} </p>
+                    <p> ${e.content} </p> 
                 </a>
             `
         })
